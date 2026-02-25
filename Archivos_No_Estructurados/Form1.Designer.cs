8d8d6fa78ef8bb3a214a4ade44bb8fee522407a9
luancs32 <I24050020@monclova.tecnm.mx>
2026-02-25 01:34:57 -0600

    Nombre

--- Archivos_No_Estructurados/Form1.Designer.cs
+++ Archivos_No_Estructurados/Form1.Designer.cs
@@ -53,11 +53,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(277, 23);
+            this.label1.Location = new System.Drawing.Point(163, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(230, 34);
+            this.label1.Size = new System.Drawing.Size(413, 34);
             this.label1.TabIndex = 23;
-            this.label1.Text = "Archivos JSON";
+            this.label1.Text = "Archivos No Estructurados";
             // 
             // btnGuardar
             // 
